2fe79afb88f521fa1ff4fdce49718e90100a53b1
Mattpott <mattpotter562@gmail.com>
2022-11-02 15:17:14 -0400

    JSON request gets a string of text based on a query. No types yet

---
+++ test/searchTests/JSONRequestorTest.java
@@ -0,0 +1,56 @@
+package searchTests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+import org.junit.Test;
+
+import search.JSONRequestor;
+
+/**
+ * A test class for when requesting JSON through the IMDB api
+ * 
+ * @author Matthew Potter
+ * @version 11/2/2022
+ */
+public class JSONRequestorTest
+{
+  // opens the testSearch.json file which is the JSON made
+  // from the search query to all titles using query "inception 2010"
+  static File searchJSON = new File("test/searchTests/testSearch.json");
+  static String searchJSONText = "";
+  static
+  {
+    Scanner fileReader = null;
+    try
+    {
+      fileReader = new Scanner(searchJSON);
+    }
+    catch (FileNotFoundException e)
+    {
+      System.err.println("Cannot find searchJSON in test");
+    } finally {
+      if (fileReader != null) {
+        while (fileReader.hasNextLine()) {
+          searchJSONText += fileReader.nextLine();
+        }
+        if (searchJSONText == "") {
+          System.err.println("JSON text is empty");
+        }
+        fileReader.close();        
+      }
+    }
+  }
+  static String queryText = "inception 2010";
+
+  @Test
+  public void testRequestJSON()
+  {
+    JSONRequestor test1 = new JSONRequestor(queryText);
+    assertEquals(searchJSONText, test1.getStringJSON());
+  }
+
+}
