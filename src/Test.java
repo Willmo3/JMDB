b6a2f6f1a09d83c505c4795e10985b787338b20f
isemelfort65 <isemelfort@gmail.com>
2022-11-02 14:45:06 -0400

    Movie cover class with jpanel that displays the movie cover

---
+++ src/Test.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+/**
+ * Test class
+ * @author Immanuel Semelfort
+ *
+ */
+public class Test extends JFrame {
+    public Test() throws IOException {
+        this.setVisible(true);
+        this.setTitle("Movie Cover");
+        this.setSize(400, 400);
+        this.setContentPane(new Panel(new MovieCover(
+                "MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_Ratio0.6800_AL_.jpg")));
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        try {
+            Test movie = new Test();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+}
